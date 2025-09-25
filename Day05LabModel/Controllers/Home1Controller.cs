d66827b139a7c1db819c587f52358850aa656b89
LeLinh2805 <levanlinh83014@gmail.com>
2025-09-25 23:07:45 +0700

    First commit - init project

---
+++ Day05LabModel/Controllers/Home1Controller.cs
@@ -0,0 +1,23 @@
+﻿using Day05LabModel.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Day05LabModel.Controllers
+{
+    public class Home1Controller : Controller
+    {
+        private List<Employee> employees = new List<Employee>()
+    {
+        new Employee { Id = 1, FullName = "Le Van Linh", Gender = "Male", Phone = "0901234567", Email = "linh234@gmail.com", Salary = 15000000, Status = true },
+        new Employee { Id = 2, FullName = "Tran Thi quan", Gender = "Female", Phone = "0912345678", Email = "huhu123@gmail.com", Salary = 12000000, Status = false },
+        new Employee { Id = 3, FullName = "Le Van duc", Gender = "Male", Phone = "0923456789", Email = "hahau88@gmail.com", Salary = 18000000, Status = true },
+        new Employee { Id = 4, FullName = "Pham Thi Dung", Gender = "Female", Phone = "0934567890", Email = "dung11@gmail.com", Salary = 20000000, Status = true }
+    };
+
+        public IActionResult Index()
+        {
+            return View(employees);
+
+        }
+
+    }
+}
---
+++ Day05LabModel/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Day05LabModel.Models;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Day05LabModel.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
